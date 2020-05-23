4826e192872173eca989978b2a5b637e7e7da0f6
mingit55 <mingit55@gmail.com>
2020-05-23 11:35:56 +0900

    add shopping cart event

--- js/App.js
+++ js/App.js
@@ -6,6 +6,7 @@ class App {
         this.storeElem = document.querySelector(".store-list");
         this.cartElem = document.querySelector(".cart-list");
         this.cartTotalElem = document.querySelector(".cart-total");
+        this.dropElem = document.querySelector(".drag-box");
 
         this.init();
         this.setEvent();   
@@ -33,12 +34,83 @@ class App {
     cartRender(){
         let total = this.cartList.reduce((p, c) => p + c.initial_data.price * c.buyCount, 0);
 
-        this.cartElem.innerHTML = "";
-        this.cartList.forEach(product => {
-            this.cartElem.append(product.cartElem);
-        });
+        this.cartElem.innerHTML = `<div class="cart-head d-flex text-center text-muted border-bottom py-3">
+                                        <div class="image">이미지</div>
+                                        <div class="info text-center">상품 정보</div>
+                                        <div class="price">가격</div>
+                                        <div class="count">구매 개수</div>
+                                        <div class="total">총합</div>
+                                    </div>`;
+        if(this.cartList.length > 0) {
+            this.cartList.forEach(product => {
+                this.cartElem.append(product.cartElem);
+            });
+        } else {
+            this.cartElem.append(this.createElem("<p class='text-center py-4 text-muted'>장바구니에 담긴 상품이 없습니다<p>"));
+        }
+        
 
         this.cartTotalElem.innerText = total.toLocaleString();
+    }
+
+    setEvent(){
+        // # 스토어 영역
+        let dragTarget, startPoint;
+        $(window).on("mousemove", e => {
+            if(e.which === 1 && dragTarget) {
+                let x = e.pageX - startPoint[0];
+                let y = e.pageY - startPoint[1];
+                dragTarget.style.transform = `translate(${x}px, ${y}px)`;
+            }
+        });
+
+        $(window).on("mouseup", e => {
+            if(e.which !== 1 || !dragTarget) return false;
+
+            let target = dragTarget;
+            target.classList.remove("active");
+            target.style.transform = "translate(0, 0)";
+            dragTarget = startPoint = null;
+
+
+            let {left, top} = $(this.dropElem).offset();
+            let width = $(this.dropElem).width();
+            let height = $(this.dropElem).height();
+
+            if(left < e.pageX && e.pageX < left + width && top < e.pageY && e.pageY < top + height){
+                // # 상품 이미지가 드롭 영역 안에 떨궈짐
+                
+                let id = target.dataset.id;
+                let product = this.storeList.find(product => product.id == id);
+                if( this.cartList.some(cartItem => cartItem == product) ) {
+                    alert("이미 장바구니에 담긴 상품입니다.");
+                } else {
+                    product.buyCount++;
+                    product.cartRender();
+                    this.cartList.push(product);
+                    this.cartRender();
+                }
+            }
+        });
+
+        $(this.storeElem).on("dragstart", ".store-item .image", e => {
+            e.preventDefault();
+            dragTarget = e.currentTarget;
+            dragTarget.classList.add("active");
+            startPoint = [e.pageX, e.pageY];
+        });
+
+
+        // # 장바구니 영역
+        $(this.cartElem).on("input", ".c-count", e => {
+            let id = e.target.dataset.id;
+            let value = e.currentTarget.value ? e.currentTarget.value : 0;
+            let product = this.cartList.find(cartItem => cartItem.id == id);
+            product.buyCount = parseInt(value);
+            product.cartRender();
+            this.cartRender();
+            e.currentTarget.focus();
+        }); 
     }
 
     getJSON(){
--- js/Product.js
+++ js/Product.js
@@ -23,20 +23,22 @@ class Product {
 
     storeRender(){
         if(!this.storeElem){
-            this.storeElem = this.app.createElem(`<div class="col-lg-4 col-md-4 col-12 mb-5">
+            this.storeElem = this.app.createElem(`<div class="col-lg-4 col-md-6 col-12 mb-5">
                                                     <div class="store-item" draggable="draggable">
-                                                        <div class="image">
-                                                            <img src="images/products/${this.photo}" title="상품 이미지" alt="상품 이미지">
-                                                        </div>
-                                                        <div class="p-3 d-flex justify-content-between align-items-end">
-                                                            <div>
-                                                                <div class="p-name fx-n3 text-muted">${this.name}</div>
-                                                                <div class="p-brand mt-1 fx-3">${this.brand}</div>
+                                                        <div>
+                                                            <div class="image" data-id=${this.id}>
+                                                                <img src="images/products/${this.photo}" title="상품 이미지" alt="상품 이미지">
                                                             </div>
-                                                            <div>
+                                                            <div class="p-3 d-flex justify-content-between align-items-end">
                                                                 <div>
-                                                                    <span class="p-price fx-5 text-red">${this.price.toLocaleString()}</span>
-                                                                    <span class="ml-1">원</span>
+                                                                    <div class="p-name fx-n3 text-muted">${this.name}</div>
+                                                                    <div class="p-brand mt-1 fx-3">${this.brand}</div>
+                                                                </div>
+                                                                <div>
+                                                                    <div>
+                                                                        <span class="p-price fx-5 text-red">${this.price.toLocaleString()}</span>
+                                                                        <span class="ml-1">원</span>
+                                                                    </div>
                                                                 </div>
                                                             </div>
                                                         </div>
@@ -50,7 +52,7 @@ class Product {
     }
 
     cartRender(){
-        let {photo, name, brand, price} = this.initial_data;
+        let {photo, product_name, brand, price} = this.initial_data;
 
         if(!this.cartElem) {
             this.cartElem = this.app.createElem(`<div class="cart-item">
@@ -59,7 +61,7 @@ class Product {
                                                     </div>
                                                     <div class="info">
                                                         <div class="px-5 w-100">
-                                                            <div class="fx-2">${name}</div>
+                                                            <div class="fx-2">${product_name}</div>
                                                             <div class="mt-1 fx-n2 text-muted">${brand}</div>
                                                         </div>
                                                     </div>
@@ -70,7 +72,7 @@ class Product {
                                                     </div>
                                                     <div class="count">
                                                         <div class="text-center">
-                                                            <input type="number" class="i-count c-count" min="0" value="${this.buyCount}">
+                                                            <input type="number" class="i-count c-count" min="0" value="${this.buyCount}" data-id="${this.id}">
                                                         </div>
                                                     </div>
                                                     <div class="total">
